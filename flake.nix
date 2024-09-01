09c79d27fba3ee9cc8c22168c106df44d4099b75
Taylor Hanken <tghanken@gmail.com>
2024-09-01 15:22:12 -0700

    Initial folder refactor

--- flake.nix
+++ flake.nix
@@ -19,9 +19,8 @@
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
-          ./apps/steam.nix
-
+          ./machines/nixos-thinkpad/configuration.nix
+          
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
@@ -29,7 +28,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
-            home-manager.users.tghanken = import ./home.nix;
+            home-manager.users.tghanken = import ./common/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
--- machines/nixos-thinkpad/configuration.nix
+++ machines/nixos-thinkpad/configuration.nix
@@ -8,13 +8,14 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../../apps/steam.nix
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos-thinkpad"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
