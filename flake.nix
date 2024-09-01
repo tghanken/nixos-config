eddf030c235fd3142e860b8a6aa1fb80d6cdcd81
Taylor Hanken <tghanken@gmail.com>
2024-09-01 12:35:58 -0700

    Add steam

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          ./apps/steam.nix
 
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
