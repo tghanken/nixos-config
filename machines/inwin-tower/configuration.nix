4a98d775149741fdf32067e475a8b06770e35865
Taylor Hanken <tghanken@gmail.com>
2024-09-06 19:50:21 -0700

    Revert "Remove user from inwin-tower"
    This reverts commit e2a6c77fc931e357ac8857692974260f68abe73c.

--- machines/inwin-tower/configuration.nix
+++ machines/inwin-tower/configuration.nix
@@ -34,6 +34,13 @@
     package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tghanken = {
+    isNormalUser = true;
+    description = "Taylor Hanken";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
