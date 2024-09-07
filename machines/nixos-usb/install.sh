7aef9ca2d56771850e2ba853f37530a5f1812fb5
Taylor Hanken <tghanken@gmail.com>
2024-09-07 12:02:09 -0700

    Update dd command in install script

--- machines/nixos-usb/install.sh
+++ machines/nixos-usb/install.sh
@@ -1,5 +1,5 @@
 nom build .#nixosConfigurations.nixos-usb.config.system.build.isoImage;
 
-dd if=result/iso/*.iso of=/dev/sdX status=progress;
+sudo dd if=./result/iso/*.iso of=/dev/sdX status=progress;
 
 sync;
