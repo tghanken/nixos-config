f734bdf2dba6b35d1a47aba27f2157a9ce77ccc2
Taylor Hanken <tghanken@gmail.com>
2024-09-08 19:47:06 -0700

    Add gparted

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -45,6 +45,7 @@
     yq-go # yaml processor https://github.com/mikefarah/yq
     eza # A modern replacement for ‘ls’
     fzf # A command-line fuzzy finder
+    gparted
 
     # networking tools
     mtr # A network diagnostic tool
