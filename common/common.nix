45756629af7dce9525530a69744969e1bd019137
Taylor Hanken <tghanken@gmail.com>
2024-09-13 11:05:11 -0700

    Add nix cache substituters (#6)

--- common/common.nix
+++ common/common.nix
@@ -14,7 +14,18 @@
 
   environment.variables.EDITOR = "nano";
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.garnix.io"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
   environment.systemPackages = with pkgs; [
     git
     nano
