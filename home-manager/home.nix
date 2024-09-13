77c9df033a75f32eb7c3d39cb4d95d810c7ee16a
Taylor Hanken <tghanken@gmail.com>
2024-09-12 18:38:52 -0700

    Add gitprune alias

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -151,6 +151,7 @@
     shellAliases = {
       osbuild = "nixos-rebuild build";
       osswitch = "sudo nixos-rebuild switch";
+      gitprune = "git fetch -p ; git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D";
     };
   };
 
