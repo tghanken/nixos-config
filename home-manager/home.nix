536432ac588ef16824df09bc23caf916469cc583
Taylor Hanken <tghanken@gmail.com>
2024-09-13 11:10:05 -0700

    Add gitsync alias (#7)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -157,6 +157,7 @@
       osbuild = "nixos-rebuild build";
       osswitch = "sudo nixos-rebuild switch";
       gitprune = "git fetch -p ; git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D";
+      gitsync = "git checkout main; git pull; gitprune;";
     };
   };
 
