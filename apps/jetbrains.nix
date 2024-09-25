847b7a21e87102a81a32146e61e5051fd43dad76
Taylor Hanken <tghanken@gmail.com>
2024-09-25 11:07:54 -0700

    Add rust dependencies for jetbrains (#13)

--- apps/jetbrains.nix
+++ apps/jetbrains.nix
@@ -1,5 +1,11 @@
 {pkgs, ...}: {
-  environment.systemPackages = [pkgs.jetbrains-toolbox];
+  environment.systemPackages = with pkgs; [
+    jetbrains-toolbox
+    jetbrains.rust-rover
+    rustup
+    clang
+    mold
+  ];
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     SDL
