9db2279246517707ff2c7c0f56acb5c23097e6f8
Taylor Hanken <tghanken@gmail.com>
2024-10-01 21:51:44 -0700

    add nixos-thinkpad key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   inwin-tower = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/iE8w8saXDau1F/BQ5IktJPQO3MhRT1+1e5UsQt/n0";
-  nixos-thinkpad = "";
+  nixos-thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEiccufbIo8bYbn5n7PpR1IAFmup53P6nn8IyYfkJfd0";
   machines = [inwin-tower nixos-thinkpad];
 in {
   "tailscale_key.age".publicKeys = machines;
