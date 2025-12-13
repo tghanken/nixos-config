c09eb7d0d680e34bb8910e8cb44c2a886477d0a7
Taylor Hanken <tghanken@gmail.com>
2025-12-13 14:46:34 +0000

    feat: add brontes (#69)

---
+++ nix/hosts/brontes/configuration.nix
@@ -0,0 +1,36 @@
+{
+  inputs,
+  flake,
+  ...
+}: {
+  imports = [
+    # Standard nixos-anywhere modules
+    inputs.disko.nixosModules.disko
+    inputs.nixos-facter-modules.nixosModules.facter
+    {
+      config.facter.reportPath =
+        if builtins.pathExists ./facter.json
+        then ./facter.json
+        else throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
+    }
+
+    # Nixos hardware additions
+    inputs.nixos-hardware.nixosModules.gmktec-nucbox-g3-plus
+
+    # Additional NixOs modules from this flake
+    flake.nixosModules.bootloader
+    flake.nixosModules.bootstrap
+    flake.nixosModules.kernel
+    flake.nixosModules.networking
+    flake.nixosModules.tailscale
+
+    flake.modules.secrets.base
+  ];
+
+  # Required for nixos-anywhere
+  disko.devices = import ./disk-config.nix;
+  networking.hostName = "brontes";
+  networking.hostId = "b6ca56e1"; # Generate using `head -c 8 /etc/machine-id`
+
+  system.stateVersion = "25.11"; # initial nixos state
+}
