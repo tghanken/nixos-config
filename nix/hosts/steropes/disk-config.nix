c156f032255eb2b8d2e40be876b3756b467bba04
Taylor Hanken <tghanken@gmail.com>
2025-12-14 20:34:25 +0000

    feat: add steropes

---
+++ nix/hosts/steropes/disk-config.nix
@@ -0,0 +1,75 @@
+{
+  disk = {
+    boot = {
+      type = "disk";
+      device = "/dev/disk/by-id/mmc-TWSC_0x29214bef";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+            priority = 1;
+          };
+          zfs = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "zroot";
+            };
+            priority = 2;
+          };
+        };
+      };
+    };
+  };
+  zpool = {
+    zroot = {
+      type = "zpool";
+      mode = {
+        topology = {
+          type = "topology";
+          cache = [];
+          vdev = [
+            {
+              members = ["boot"];
+            }
+          ];
+        };
+      };
+      rootFsOptions = {
+        xattr = "sa";
+        compression = "lz4";
+        atime = "off";
+        recordsize = "64K";
+        "com.sun:auto-snapshot" = "true";
+      };
+      mountpoint = "/";
+      datasets = {
+        nix = {
+          type = "zfs_fs";
+          mountpoint = "/nix";
+          options."com.sun:auto-snapshot" = "false";
+        };
+        var = {
+          type = "zfs_fs";
+          mountpoint = "/var";
+        };
+        home = {
+          type = "zfs_fs";
+          mountpoint = "/home";
+        };
+        reserved = {
+          type = "zfs_fs";
+          options.refreservation = "10G";
+          options.mountpoint = "none";
+        };
+      };
+    };
+  };
+}
