ab7b8119f0e68b66f0560f03a8f949e5958c3da5
Taylor Hanken <tghanken@gmail.com>
2025-12-14 21:35:25 +0000

    feat: add arges

---
+++ nix/hosts/arges/configuration.nix
@@ -0,0 +1,43 @@
+{
+  inputs,
+  flake,
+  ...
+}: {
+  imports = [
+    # Standard nixos-anywhere modules
+    inputs.disko.nixosModules.disko
+    inputs.nixos-facter-modules.nixosModules.facter
+    {
+      config.facter.reportPath =
+        if builtins.pathExists ./facter.json
+        then ./facter.json
+        else throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
+    }
+
+    # Nixos hardware additions
+    inputs.nixos-hardware.nixosModules.gmktec-nucbox-g3-plus
+
+    # Add user modules
+    flake.modules.users.tghanken
+
+    # Additional NixOs modules from this flake
+    flake.nixosModules.bootloader
+    flake.nixosModules.bootstrap
+    flake.nixosModules.determinate
+    flake.nixosModules.kernel
+    flake.nixosModules.networking
+    flake.nixosModules.tailscale
+
+    flake.modules.secrets.base
+
+    flake.modules.utils.auto-upgrade
+    flake.modules.utils.earlyoom
+  ];
+
+  # Required for nixos-anywhere
+  disko.devices = import ./disk-config.nix;
+  networking.hostName = "arges";
+  networking.hostId = "be6e3de2"; # Generate using `head -c 8 /etc/machine-id`
+
+  system.stateVersion = "25.11"; # initial nixos state
+}
