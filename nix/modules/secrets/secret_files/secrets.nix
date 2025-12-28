bc6727b9fc9d56cdfaf8ae85f103a5bae08a7bf7
Taylor Hanken <tghanken@gmail.com>
2025-12-28 00:27:13 +0000

    fix: allow all netbird keys to decrypt

--- nix/modules/secrets/secret_files/secrets.nix
+++ nix/modules/secrets/secret_files/secrets.nix
@@ -2,9 +2,12 @@ let
   keys = import ../keys.nix;
 in {
   "encrypted/github_pat.age".publicKeys = keys.all;
-  "encrypted/netbird_taylor_client.age".publicKeys = keys.clients ++ keys.tghanken;
-  "encrypted/netbird_local_server.age".publicKeys = keys.local-servers ++ keys.tghanken;
-  "encrypted/netbird_remote_server.age".publicKeys = keys.remote-servers ++ keys.tghanken;
+  "encrypted/netbird_taylor_client.age".publicKeys = keys.all;
+  "encrypted/netbird_local_server.age".publicKeys = keys.all;
+  "encrypted/netbird_remote_server.age".publicKeys = keys.all;
+  # "encrypted/netbird_taylor_client.age".publicKeys = keys.clients ++ keys.tghanken;
+  # "encrypted/netbird_local_server.age".publicKeys = keys.local-servers ++ keys.tghanken;
+  # "encrypted/netbird_remote_server.age".publicKeys = keys.remote-servers ++ keys.tghanken;
   "encrypted/nix_store_signing_key.age".publicKeys = keys.all;
   "encrypted/tailscale_key.age".publicKeys = keys.all;
 }
