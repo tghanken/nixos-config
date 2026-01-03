50d77186c1605a16c3ba807b2a4e22a1f08f9925
Taylor Hanken <tghanken@gmail.com>
2026-01-03 17:00:06 +0000

    chore: switch from netbird to tailscale (#92)

--- nix/modules/profiles/remote-server.nix
+++ nix/modules/profiles/remote-server.nix
@@ -9,6 +9,6 @@ with config; {
     flake.modules.profiles.shared-server
   ];
 
-  age.secrets.netbird_key.file = ../secrets/secret_files/encrypted/netbird_local_server.age;
-  services.netbird_user.auth_key_path = age.secrets.netbird_key.path;
+  # age.secrets.netbird_key.file = ../secrets/secret_files/encrypted/netbird_local_server.age;
+  # services.netbird_user.auth_key_path = age.secrets.netbird_key.path;
 }
