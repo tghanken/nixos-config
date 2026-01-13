71aba2170e5c4fdc40ecb1060fff06f353916f09
Taylor Hanken <tghanken@gmail.com>
2026-01-13 12:49:26 -0800

    chore: update flake.lock

--- nix/modules/nixos/kernel.nix
+++ nix/modules/nixos/kernel.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
   # Fixes broken zfs package. See https://github.com/NixOS/nixos-hardware/issues/1675
-  boot.kernelPackages = pkgs.linuxPackages_6_17;
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
+  boot.zfs.package = pkgs.zfs_2_4;
 }
